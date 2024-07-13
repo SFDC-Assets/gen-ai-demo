2a37fd53522f4c51cf5ee815e66248349ad0cd3a
zhou059 <zhou059@gmail.com>
2024-07-13 09:20:36 -0700

    tune

--- orgInit.sh
+++ orgInit.sh
@@ -5,7 +5,7 @@
 # sfdx automig:dump -d data -o Account,Contact,Case,Lead,Opportunity,Phantom__c,Prompt_Search_Term_v1__c,Search_Term__c,Template_Text__c,Workshop_Config_Steps__c,Workshop__c
 sfdx shane:org:create -f config/partner-scratch-def.json -d 30 -s --wait 60 --userprefix einstein -o gpt.demo
 
-# npm install
+npm install
 node run.js
 sleep 120
 
