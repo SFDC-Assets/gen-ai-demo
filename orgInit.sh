0707111d205407aeaab761b03545fd01eea80320
zhou059 <zhou059@gmail.com>
2024-07-12 21:59:34 -0700

    cleanup

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@
 
 # https://hosted-scratch.herokuapp.com/launch?template=https://github.com/SFDC-Assets/gen-ai-demo/tree/main
 # sfdx force:source:retrieve -m GenAiPromptTemplate,GenAiPromptTemplateActv,GenAiFunction,GenAiPlanner
-# sfdx automig:dump -d data -o Account,Contact,Case,Lead,Phantom__c,Prompt_Search_Term_v1__c,Search_Term__c,Template_Text__c,Workshop_Config_Steps__c,Workshop__c
+# sfdx automig:dump -d data -o Account,Contact,Case,Lead,Opportunity,Phantom__c,Prompt_Search_Term_v1__c,Search_Term__c,Template_Text__c,Workshop_Config_Steps__c,Workshop__c
 sfdx shane:org:create -f config/partner-scratch-def.json -d 30 -s --wait 60 --userprefix einstein -o gpt.demo
 
 npm install
