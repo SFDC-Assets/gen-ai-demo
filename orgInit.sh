1cd9301b96cfcabb11b76bc39985a637391b5e96
zhou059 <zhou059@gmail.com>
2024-07-22 15:03:17 -0700

    data trials

--- orgInit.sh
+++ orgInit.sh
@@ -2,22 +2,22 @@
 
 # https://hosted-scratch.herokuapp.com/launch?template=https://github.com/SFDC-Assets/gen-ai-demo/tree/main
 # sfdx force:source:retrieve -m GenAiPromptTemplate,GenAiPromptTemplateActv,GenAiFunction,GenAiPlanner
-# sfdx automig:dump -d data -o Account,Contact,Case,Lead,Opportunity,Phantom__c,Prompt_Search_Term_v1__c,Search_Term__c,Template_Text__c,Workshop_Config_Steps__c,Workshop__c
-sfdx shane:org:create -f config/fscai-scratch-def.json -d 30 -s --wait 60 --userprefix einstein -o gpt.demo
+# sfdx automig:dump -d data -o Account,Contact,Case,Lead,Opportunity,FinServ__FinancialAccount__c,PersonLifeEvent,Claim,InsurancePolicy,InsurancePolicyCoverage,InsurancePolicyAsset
+sfdx shane:org:create -f config/fscai-scratch-def.json -d 30 -s --wait 60 --userprefix fsc -o einstein.gpt
 
 npm install
 node run.js
 sleep 120
 
 sfdx shane:user:password:set -p salesforce1 -g User -l User
-
-sfdx force:user:permset:assign -n EinsteinGPTPromptTemplateManager
-sfdx force:user:permset:assign -n EinsteinGPTPromptTemplateUser
-sfdx shane:user:permset:assign -l User -g User -n CDPAdmin
+sfdx force:package:install --package=04t4W000003ChfP --wait 20 -r
+sfdx force:package:install --package=04tHn000001dvY4 --wait 30
+sfdx force:package:install --package=04t1E000001Iql5 --wait 2
+sfdx force:user:permset:assign -n FinancialServicesCloudStandard
+sfdx force:user:permset:assign -n FinancialServicesCloudExtension
 
 sfdx force:source:push
 
-sfdx force:user:permset:assign -n Heroku_Endpoint_Access
 sfdx force:user:permset:assign -n TDX_Demo_Build_Permissions
 
 sfdx automig:load -d data
