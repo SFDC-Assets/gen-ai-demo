793360c818f3c4fe4a49d631124848397868cc86
zhou059 <zhou059@gmail.com>
2024-08-05 21:31:35 -0700

    diff deploy

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,8 @@
 # https://hosted-scratch.herokuapp.com/launch?template=https://github.com/SFDC-Assets/gen-ai-demo/tree/main
 # sfdx force:source:retrieve -m GenAiPromptTemplate,GenAiPromptTemplateActv,GenAiFunction,GenAiPlanner
 # sfdx automig:dump -d data -o Account,Contact,Case,Lead,Opportunity,Phantom__c,Prompt_Search_Term_v1__c,Search_Term__c,Template_Text__c,Workshop_Config_Steps__c,Workshop__c
+sfdx update
+
 sfdx shane:org:create -f config/partner-scratch-def.json -d 30 -s --wait 60 --userprefix einstein -o gpt.demo
 
 npm install
@@ -14,8 +16,6 @@ sfdx shane:user:password:set -p salesforce1 -g User -l User
 
 sfdx force:user:permset:assign -n EinsteinGPTPromptTemplateManager
 sfdx force:user:permset:assign -n EinsteinGPTPromptTemplateUser
-
-npm update --global @salesforce/cli
 
 sfdx force:source:push
 
